ce0f093e2493ef5bcf34f4230c2eb1633cf07b62
tarappo <tarappo@gmail.com>
2020-02-19 17:43:58 +0900

    init files

---
+++ uitest-sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  uitest-sample
+//
+//  Created by thirata on 2020/02/19.
+//  Copyright © 2020 thirata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
