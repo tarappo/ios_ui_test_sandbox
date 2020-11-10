aca32ead20ccc2cd5633f33f9baaf2af13188f2e
Toshiyuki Hirata <toshiyuki.hirata@dena.com>
2020-11-10 11:26:13 +0900

    テストケースの追加

--- uitest-sampleUITests/QueryUITest.swift
+++ uitest-sampleUITests/QueryUITest.swift
@@ -11,12 +11,15 @@ import XCTest
 // 実際のUIテストの実装パターンとは異なる点に注意してください
 class QueryUITest: XCTestCase {
     let app = XCUIApplication()
-    let view1labelCount = 3
-    let view2LabelCount = 2
+    let label1Count = 3
+    let label2Count = 2 // View2内
+    let label3Count = 1 // 画面外
     let buttonCount = 2
     let switchCount = 1
     let sliderCount = 1
     let textFieldCount = 1
+    let view1Count = 1
+    let view2Count = 1
     
     override func setUp() {
         continueAfterFailure = false
@@ -31,11 +34,11 @@ class QueryUITest: XCTestCase {
     func testLabelCount() {
         //MARK: iOS13系だとstaticTextsをカウントするとButtonまでカウントされる
         if #available(iOS 13.0, *) {
-            let totalCount = view1labelCount + view2LabelCount + buttonCount
+            let totalCount = label1Count + label2Count + label3Count + buttonCount
             XCTAssertEqual(totalCount, app.staticTexts.count, app.debugDescription)
         }else {
         //MARK: iOS12の実機の場合、statusbarにある時刻の箇所がstaticTextとしてカウントされる
-            let totalCount = view1labelCount + view2LabelCount
+            let totalCount = label1Count + label2Count + label3Count
             XCTAssertEqual(totalCount, app.otherElements["main_view_1"].descendants(matching: .staticText).count, app.debugDescription)
         }
     }
@@ -58,9 +61,14 @@ class QueryUITest: XCTestCase {
     
     //MARK: - Exists
     func testLabelExists() {
-        let totalCount = view1labelCount + view2LabelCount
-        for no in 1...totalCount {
-            XCTAssertTrue(app.staticTexts["main_label_\(no)"].exists)
+        for no in 1...label1Count {
+            XCTAssertTrue(app.staticTexts["main_label1_\(no)"].exists)
+        }
+        for no in 1...label2Count {
+            XCTAssertTrue(app.staticTexts["main_label2_\(no)"].exists)
+        }
+        for no in 1...label3Count {
+            XCTAssertTrue(app.staticTexts["main_label3_\(no)"].exists)
         }
     }
 
@@ -87,12 +95,24 @@ class QueryUITest: XCTestCase {
             XCTAssertTrue(app.textFields["main_textfield_\(no)"].exists)
         }
     }
-    
+
+    func testViewExists() {
+        let viewCount = view1Count + view2Count
+        for no in 1...viewCount {
+            XCTAssertTrue(app.otherElements["main_view_\(no)"].exists)
+        }
+    }
+
     //MARK: - Hittable
     func testLabelHittable() {
-        let totalCount = view1labelCount + view2LabelCount
-        for no in 1...totalCount {
-            XCTAssertTrue(app.staticTexts["main_label_\(no)"].isHittable)
+        for no in 1...label1Count {
+            XCTAssertTrue(app.staticTexts["main_label1_\(no)"].isHittable)
+        }
+        for no in 1...label2Count {
+            XCTAssertTrue(app.staticTexts["main_label2_\(no)"].isHittable)
+        }
+        for no in 1...label3Count {
+            XCTAssertFalse(app.staticTexts["main_label3_\(no)"].isHittable)
         }
     }
 
@@ -114,11 +134,17 @@ class QueryUITest: XCTestCase {
         }
     }
 
-    //MARK: - false: isHittable
     func testSliderHittable() {
         for no in 1...sliderCount {
             XCTAssertTrue(app.sliders["main_slider_\(no)"].isHittable)
         }
+    }
+
+    func testViewHittable() {
+        // main_view_1の「User Interaction Enabled」がtrueになっています
+        XCTAssertTrue(app.otherElements["main_view_1"].isHittable)
+        // main_view_2の「User Interaction Enabled」がfalseになっています
+        XCTAssertFalse(app.otherElements["main_view_2"].isHittable)
     }
 }
 
